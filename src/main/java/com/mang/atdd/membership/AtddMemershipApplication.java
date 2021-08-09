eddb5a60d9a8accd3663f97aff132ff0fdd79bec
MangKyu <whalsrb1226@naver.com>
2021-08-09 16:52:07 +0900

    Initial Commit

---
+++ src/main/java/com/mang/atdd/membership/AtddMemershipApplication.java
@@ -0,0 +1,13 @@
+package com.mang.atdd.membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddMemershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtddMemershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mang/atdd/membership/AtddMemershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mang.atdd.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddMemershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
