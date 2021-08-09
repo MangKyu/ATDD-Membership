2340528a28b1ca130280a4d5ee0e4a1a858bb92a
MangKyu <whalsrb1226@naver.com>
2021-08-09 19:59:12 +0900

    사용자아이디와 멤버십 타입으로 조회하는 코드 및 테스트 작성

--- src/main/java/com/mang/atdd/membership/app/membership/repository/MembershipRepository.java
+++ src/main/java/com/mang/atdd/membership/app/membership/repository/MembershipRepository.java
@@ -1,8 +1,11 @@
 package com.mang.atdd.membership.app.membership.repository;
 
+import com.mang.atdd.membership.app.enums.MembershipType;
 import com.mang.atdd.membership.app.membership.entity.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+
+    Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
 
 }
--- src/test/java/com/mang/atdd/membership/app/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/mang/atdd/membership/app/membership/repository/MembershipRepositoryTest.java
@@ -35,4 +35,25 @@ public class MembershipRepositoryTest {
         assertThat(result.getPoint()).isEqualTo(10000);
     }
 
+    @Test
+    public void 멤버십이존재하는지테스트() {
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipType(MembershipType.NAVER)
+                .point(10000)
+                .build();
+
+        // when
+        membershipRepository.save(membership);
+        final Membership findResult = membershipRepository.findByUserIdAndMembershipType("userId", MembershipType.NAVER);
+
+        // then
+        assertThat(findResult).isNotNull();
+        assertThat(findResult.getId()).isNotNull();
+        assertThat(findResult.getUserId()).isEqualTo("userId");
+        assertThat(findResult.getMembershipType()).isEqualTo(MembershipType.NAVER);
+        assertThat(findResult.getPoint()).isEqualTo(10000);
+    }
+
 }
