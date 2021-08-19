1b8fa9782598d1a2dd76b2fea8195dbfcadd3ac9
MangKyu <whalsrb1226@naver.com>
2021-08-19 22:26:35 +0900

    DefaultRestController 추가 메소드 오버라이딩 구현

--- src/main/java/com/mang/atdd/membership/app/common/DefaultRestController.java
+++ src/main/java/com/mang/atdd/membership/app/common/DefaultRestController.java
@@ -5,9 +5,17 @@ import com.mang.atdd.membership.exception.MembershipException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public abstract class DefaultRestController extends ResponseEntityExceptionHandler {
@@ -27,6 +35,28 @@ public abstract class DefaultRestController extends ResponseEntityExceptionHandl
     private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final MembershipErrorResult errorResult) {
         return ResponseEntity.status(errorResult.getHttpStatus())
                 .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException ex,
+            final HttpHeaders headers,
+            final HttpStatus status,
+            final WebRequest request) {
+
+        final List<String> errorList = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        log.warn("Invalid DTO Parameter errors : {}", errorList);
+        return this.makeErrorResponseEntity(errorList.toString());
+    }
+
+    private ResponseEntity<Object> makeErrorResponseEntity(final String errorDescription) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), errorDescription));
     }
 
     @Getter
